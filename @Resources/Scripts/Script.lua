410ef0f40d94ecb8c8169bb371cfce3a3368725c
Jai Sawhney <jai.sawhney1@gmail.com>
2024-03-19 22:52:15 -0700

    Add basic functionality

--- @Resources/Scripts/Script.lua
+++ @Resources/Scripts/Script.lua
@@ -1,3 +1,16 @@
 function Initialize()
-	print('Hello World')
+	FindDevices()
+	SetOutputDevice()
+
+	SKIN:Bang('!CommandMeasure','MeasureWin7Audio','SetOutputIndex  ' .. SKIN:GetVariable('SpeakerDeviceIndex'))
+end
+
+-- Initial device lookup (on refresh/load)
+function FindDevices()
+	
+end
+
+-- Update the icon
+function SetOutputDevice()
+
 end
